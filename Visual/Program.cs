9b9d016095d8b822597fd7b014643a751641d6f1
Ibrahim <xpl0dec@protonmail.com>
2024-01-26 13:20:23 +0700

    Menambahkan beberapa file

---
+++ Visual/Program.cs
@@ -0,0 +1,13 @@
+using System; 
+
+namespace HelloWorld 
+{ 
+  class helloWorld 
+  { 
+    static void Main(string[] args) 
+    { 
+      Console.WriteLine("Hai saya baru mempelajari pemrograman C#"); 
+      Console.ReadKey(); 
+    }   
+   } 
+}
